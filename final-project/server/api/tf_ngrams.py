c3d5eaacb1c2d0129cd1ab125cc44f1429dfae47
rachel fischoff <59700665+rachel-fischoff@users.noreply.github.com>
2020-06-02 15:12:19 -0400

    with function to sort list to correct order on the back end

--- final-project/server/api/tf_ngrams.py
+++ final-project/server/api/tf_ngrams.py
@@ -7,8 +7,7 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 import numpy as np
 import pandas as pd
-from collections import Counter
-import csv, json 
+import csv, json, re 
 
 from sklearn.feature_extraction.text import CountVectorizer
 
@@ -19,6 +18,7 @@ def run_ngrams():
     #open the text file 
     with open ('text.txt', 'r') as infile:
         text_analysis = [infile.read()]
+    
 
     #Use scikit learn to create the ngrams 
     vectorizer = CountVectorizer(analyzer='word', ngram_range=(1, 3), token_pattern=r'\b\w+\b', min_df=1)
@@ -75,9 +75,60 @@ def run_ngrams():
     df = df.loc[df['totalwords'] == 1]
     df = df.to_csv(r'words.csv', index = False, header=True)
 
+    #I need to arrange the words csv in the same order as the word sentence. 
+    # [create 2 arrays with indexes? I'm not sure the best way to do this]???
+    print(text_analysis, text_analysis[0], 'txt analysis + text analysis [0]')
+    #read warning?
+    ordered_list = re.sub("[^\w]", " ",  text_analysis[0].lower()).split()
+    print(ordered_list, 'ordered_list')
+    #right now the wordList is the original text broke into an array with each word as in index []
+    #maybe i should map it out ?? in the correct order?
+    #sorting lists by index? 
+    
+    
+    #use pandas to read the csv
+    df = pd.read_csv('words.csv')
+    scored_list = df.values.tolist()
+    print(scored_list, 'list')
 
-   
- 
+
+
+        # printing original list 
+    print ("The original list is : " + str(scored_list)) 
+    
+    # printing sort order list 
+    print ("The sort order list is : " + str(ordered_list)) 
+    
+    # using list comprehension 
+    # to sort according to other list  
+    res = [tuple for x in ordered_list for tuple in scored_list if tuple[0] == x] 
+    
+    # printing result 
+    print ("The sorted list is : " + str(res)) 
+
+        # Python code to demonstrate 
+    # to sort according to other list 
+    # using sort() + lambda + index() 
+
+
+
+    # printing original list 
+    print ("The original list is : " + str(scored_list)) 
+
+    # printing sort order list 
+    print ("The sort order list is : " + str(ordered_list)) 
+
+    # using sort() + lambda + index() 
+    # to sort according to other list 
+    # test_list.sort(key = lambda(i, j): sort_order.index(i)) # works in python 2 
+    scored_list.sort(key = lambda i: ordered_list.index(i[0])) # works in python 3 
+
+    # printing result 
+    print ("The sorted list is : " + str(scored_list)) 
+
+
+
+    
 
     
 if __name__ == "__main__":
